e939fabf12958f3049d6e69dec0b772cc72707e0
AndyRama <andyramaroson@gmail.com>
2021-10-15 22:15:43 +0200

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'projects#index'
+  root to: 'pages#home'
   devise_for :users  
 
   resources :projects, only:[:show, :index]
- 
+  
+  get "/projects" => 'projects#index'
+  get "/homer" => "pages#homer", as: :homer
   get "/profile" => "pages#profile", as: :profile
   get "/contact" => "pages#contact", as: :contact
- get "/test" => "pages#test", as: :test
+  get "/test" => "pages#test", as: :test
   get "/CV" => "pages#CV", as: :CV
 end
